4dbd0c97ceed6f1f4f95c36de322a89bef08a8cf
Andrew Lanham <54631753+andrewdlanham@users.noreply.github.com>
2024-11-04 22:13:17 -0500

    Changes to letters and spawn points

--- githubgameoff2024/scripts/letter_item.gd
+++ githubgameoff2024/scripts/letter_item.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var letter_label: Label = $LetterLabel
 
 var letterPosition # Letter's position in secret word
+var hiddenLetter = ''
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print("Letter ready.")
+	letter_label.text = '?'
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
